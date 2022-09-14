09a9793a070f2c797cb77668f3b052be50a9ba4e
mike28gt <miguelalejandro28@gmail.com>
2022-09-13 21:35:02 -0600

    Se agregan ejemplos

--- 2022-s10-if.py
+++ 2022-s10-if.py
@@ -16,9 +16,8 @@ a = int(a)
 b = int(b)
 c = int(c)
 
-if a != 0:
-    x1 = (-b + math.sqrt(b**2 - (4*a*c))) / (2*a)
-    x2 = (-b - math.sqrt(b**2 - (4*a*c))) / (2*a)
+x1 = (-b + math.sqrt(b**2 - (4*a*c))) / (2*a)
+x2 = (-b - math.sqrt(b**2 - (4*a*c))) / (2*a)
 
-    print("El valor de x1 es", x1)
-    print("El valor de x2 es", x2)
+print("El valor de x1 es", x1)
+print("El valor de x2 es", x2)
