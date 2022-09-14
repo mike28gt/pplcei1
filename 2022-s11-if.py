8da56046090d0dfc90f104428973e20274f17584
mike28gt <miguelalejandro28@gmail.com>
2022-09-13 21:27:47 -0600

    Script sesion 11

---
+++ 2022-s11-if.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 22 21:04:32 2022
+
+@author: miguelcatalan
+"""
+
+import math
+
+a = input("Ingrese valor para a: ")
+b = input("Ingrese valor para b: ")
+c = input("Ingrese valor para c: ")
+
+a = int(a)
+b = int(b)
+c = int(c)
+
+x1 = (-b + math.sqrt(b**2 - (4*a*c))) / (2*a)
+x2 = (-b - math.sqrt(b**2 - (4*a*c))) / (2*a)
+
+print("El valor de x1 es", x1)
+print("El valor de x2 es", x2)
