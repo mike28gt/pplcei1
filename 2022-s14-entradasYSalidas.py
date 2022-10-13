1db7fc362c47ed94a514ca73434081255f2549f9
mike28gt <miguelalejandro28@gmail.com>
2022-10-13 00:56:12 -0600

    Se agregan ejemplos

---
+++ 2022-s14-entradasYSalidas.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 17 20:36:57 2022
+
+@author: miguelcatalan
+"""
+
+"""
+edad = input("Cuál es tu edad: ")
+nombre = input("Cuál es tu nombre: ")
+anio_actual = 2022
+
+anio_nacimiento = anio_actual - int(edad)
+
+print(nombre, "nacio en el anio", anio_nacimiento)
+"""
+
+numero1 = input("Ingrese primer numero:")
+numero2 = input("Ingrese segundo numero:")
+
+suma = int(numero1) + int(numero2)
+
+print("La suma de los numeros es:", suma)
