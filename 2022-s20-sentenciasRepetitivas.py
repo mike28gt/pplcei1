a66e381bba31352d7a28062490a3f7ed41dda397
mike28gt <miguelalejandro28@gmail.com>
2022-10-13 00:12:58 -0600

    Se agregan ejemplos.

---
+++ 2022-s20-sentenciasRepetitivas.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  5 20:53:37 2022
+
+@author: miguelcatalan
+"""
+
+
+numero = 1
+print (numero)
+
+while numero < 5:
+    numero = numero + 1
+    print(numero)
+
+
+"""
+numero = 1
+print(numero)
+
+numero = numero + 1
+print(numero)
+
+numero = numero + 1
+print(numero)
+
+numero = numero + 1
+print(numero)
+
+numero = numero + 1
+print(numero)
+"""
