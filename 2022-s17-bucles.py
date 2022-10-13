10b81ae4090f6d604b31b5d4808138a509804e40
mike28gt <miguelalejandro28@gmail.com>
2022-10-13 01:05:03 -0600

    Se agregan ejemplos.

---
+++ 2022-s17-bucles.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 14 20:50:29 2022
+
+@author: miguelcatalan
+"""
+
+s = 0
+s = s + 1
+s = s + 2
+s = s + 3
+s = s + 4
+s = s + 5
+
+s = s + 999
+s = s + 1000
+print(s)
