85645eb2f57ccc302da2d34ca6bbc82fa2166cb0
mike28gt <miguelalejandro28@gmail.com>
2022-10-12 23:51:13 -0600

    Se agregan ejemplos.

---
+++ 2022-s21-sentenciasRepetitivas.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 10 21:14:01 2022
+
+@author: miguelcatalan
+"""
+
+numero = -1
+
+while numero < 0:
+    numero = float(input("Ingrese un número positivo: "))
