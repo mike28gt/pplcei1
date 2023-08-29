3927dcc528575d75238a5e651f4c9bb7710cb510
mike28gt <miguelalejandro28@gmail.com>
2023-08-28 22:59:32 -0600

    Ejemplo de clase.

---
+++ 2023/ejemplo5.py
@@ -0,0 +1,25 @@
+type(4)
+type(3.14159)
+type("Miguel")
+type(True)
+
+# Variables
+#   Son nombres que le asignamos a los datos
+#   Los datos son asignados a las variables por medio del operador de asignación =
+dias = 365
+type(dias)
+
+# Las variables se crean al momento de asignarles un dato por PRIMERA VEZ
+meses = 0
+
+# Cuando utilizamos las variables, estas son reempladas internamente por el dato que están representando
+# Los nombres de variables no pueden iniciar con un número
+# Las variables se refieren a objetos (datos/espacios de memoria) por lo que no pueden ser declaradas antes de utilizarse
+
+# Expresiones
+2 + 3
+xy - x + 2
+
+# Son una secuencia ordenada de operadores y operandos
+#   Operadores: son los símbolos que definen una operación
+#   Operandos: son los elementos que opera el operador
