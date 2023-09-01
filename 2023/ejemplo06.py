71dd7a5295c6f80346ba8fcd286add34fa6d003c
mike28gt <miguelalejandro28@gmail.com>
2023-08-31 21:21:49 -0600

    Se agrega ejemplo en clase.

---
+++ 2023/ejemplo06.py
@@ -0,0 +1,32 @@
+# Expresiones
+#   1. Aritméticas
+#       x
+#       xy 
+#       x.y
+#       2 x 4
+#       *: es el único símbolo válido para representar multiplicación
+#       /: es el único símbolo válido para representar división
+#       (): los paréntesis funcionan como símbolos de agrupación
+# Ejemplos:
+3 + 2
+numero1 = 3
+numero2 = 2
+numero1 - numero2
+numero2 * 2
+resultado = 8 / 2
+c = a + b
+#[{(2 + 3)}] <-> (((2 + 3)))
+#matemática       python
+3 + 2 * 9
+3 + 18
+21
+numero1 = 3
+numero2 = 2
+numero1 - numero2
+numero2 * 2
+x = 1
+(2*1/(1+3))+(5/1)-4
+(2*x)/(x+3)+5/x-4
+
+#   2. Relacionales
+#   3. Lógicas
