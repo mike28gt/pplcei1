30a75780efccb431ec4ee32e419ce7887a46e2f3
mike28gt <miguelalejandro28@gmail.com>
2023-09-06 23:10:51 -0600

    Nuevos ejemplos.

---
+++ 2023/ejemplo8.py
@@ -0,0 +1,11 @@
+operando = input()
+operando_entero = int(operando)
+multiplicacion = 5 * operando_entero
+print(multiplicacion)
+
+# Conversión/casteo de tipos de datos
+#   consisten en convertir un dato de un tipo específico a otro tipo de dato diferente.
+#       int() : convierte un dato a tipo entero
+#       float() : convierte un dato a tipo flotante (reales)
+#       bool(): convierte un dato a tipo booleano
+#       str(): convierte un dato a tipo cadena
---
+++ 2023/ejemplo8_2.py
@@ -0,0 +1,7 @@
+
+primer_numero = input("Ingrese el primer número a sumar: ")
+primer_numero_entero = int(primer_numero)
+segundo_numero = input("Ingrese el segundo número a sumar: ")
+segundo_numero_entero = int(segundo_numero)
+suma = primer_numero_entero + segundo_numero_entero
+print("El resultado de sumar los dos números es",suma)
