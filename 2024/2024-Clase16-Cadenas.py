16c9cb74ad09278597b102525524afd8442e7d9e
Miguel Catalan <miguelcatalan@MacBook-Pro.local>
2024-09-27 17:33:15 -0600

    Actualización al ejemplo.

---
+++ 2024/2024-Clase16-Cadenas.py
@@ -0,0 +1,12 @@
+'''
+    El tipo de dato texto también es llamado cadena. Las cadenas están compuestas de caracteres, por lo qu4e se puede decir que una
+    cadena es un conjunto de caracteres. Este conjunto es un conjunto ordenado, lo cual significa que cada uno de sus elementos ocupa una 
+    posición en específico dentro del conjunto.
+    
+    La posición que ocupan los caracteres se identifican por medio de número enteros, en donde el primer elemento del conjunto tiene asignada
+    la posición 0, el segundo elemento la posición 1, el siguiente la posición 3, y así consecutivamente. Cada uno de estos números enteros reciben
+    el nombre de índices.
+
+    Inmutabilidad es la propiedad que tienen la cadenas de no poder ser modificadas. Cada que vez que se realiza alguna operación sobre 
+    una cadena, el resultado será una nueva cadena.
+'''
