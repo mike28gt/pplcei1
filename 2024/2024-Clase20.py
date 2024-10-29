6a94cfd923b669451ce36620b45b3dcd07c95b17
Miguel Catalan <miguelcatalan@MacBook-Pro.local>
2024-10-28 23:02:22 -0600

    Ejemplos.

---
+++ 2024/2024-Clase20.py
@@ -0,0 +1,15 @@
+# Ejemplo 1
+"""
+numero = input("Ingrese un número real: ")
+numero = float(numero)
+
+if numero > 0:
+    print("El número es positivo")
+"""
+
+# Ejemplo 2
+edad = input("Ingrese una edad: ")
+edad = int(edad)
+
+if edad > 18:
+    print("Eres mayor de edad")
